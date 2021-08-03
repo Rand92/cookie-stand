3c618933770bffc6db92788c7c954ca903f1073e
Rand92 <s.rund@yahoo.com>
2021-08-03 22:10:01 +0300

    css applied

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ let hours = ['','6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm'
 let secsalmonCookies = document.getElementById('salmonCookies');
 const tableEl = document.createElement('table');
 secsalmonCookies.appendChild(tableEl);
-
+// new function
 let tr1 = document.createElement('tr');
 tableEl.appendChild(tr1);
 
@@ -14,6 +14,7 @@ for (let i = 0; i < hours.length; i++) {
     th1.textContent = hours[i];
     tr1.appendChild(th1);
 }
+
 let column= [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] ;
 
 function Salmon(barchName, minCust, maxCust, AvgCookie) {
@@ -78,162 +79,3 @@ let Seattle = new Salmon('Seattle', 23, 65, 6.5);
     Paris.render();
     Lima.render();
     footer();
-
-// console.log (Seattle);
-
-// let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-// const Seattle = {
-//     barchName : 'Seattle',
-//     minCust: 23,
-//     maxCust: 65,
-//     AvgCookie: 6.5,
-//     cookies :0,
-//     total :0 ,
-//     // RandomNumOfCust : means the avg num of cookies sold/hour 
-//     // cookies : avg cookies sold * Random number of customers
-//     RandomNumOfCust: function() {
-//         let min = Math.ceil(this.minCust);
-//         let max = Math.floor(this.maxCust);
-//         this.cookies = Math.floor((Math.random() * (max - min + 1) + min)*this.AvgCookie); //The maximum is inclusive and the minimum is inclusive
-//      this.total += this.cookies
-//      return this.cookies ;
-//     }, 
-
-//     render: function () { 
-
-// let secsalmonCookies = document.getElementById('salmonCookies');
-//        let h2Element = document.createElement('h2');
-//        h2Element.textContent = this.barchName;
-//        secsalmonCookies.appendChild(h2Element);
-
-//     let ulEl = document.createElement('ul');
-//         secsalmonCookies.appendChild(ulEl);
-
-//     for(let i = 0; i < hours.length; i++) {
-//         let liElement = document.createElement('li');
-//       liElement.textContent = hours[i]+': '+this.RandomNumOfCust() + 'Cookies';
-//       ulEl.appendChild(liElement);
-//     }
-//     let liElement = document.createElement('li');
-//     liElement.textContent = 'the sum of these hourly totals Cookies: '+this.total+ 'Cookies';
-//     ulEl.appendChild(liElement);
-//     }
-
-// }
-// Seattle.render();
-
-// const Tokyo = {
-//     barchName : 'Tokyo',
-//     minCust: 3,
-//     maxCust: 24,
-//     AvgCookie: 1.2,
-//     cookies :0,
-//     total :0 ,
-//     RandomNumOfCust: function() {
-//         let min = Math.ceil(this.minCust);
-//         let max = Math.floor(this.maxCust);
-//         this.cookies = Math.floor((Math.random() * (max - min + 1) + min)*this.AvgCookie);//The maximum is inclusive and the minimum is inclusive
-//      this.total += this.cookies
-//      return this.cookies ;
-//     }, 
-
-
-//     render: function () { 
-
-// let secsalmonCookies = document.getElementById('salmonCookies');
-// let h2Element = document.createElement('h2');
-// h2Element.textContent = this.barchName;
-// secsalmonCookies.appendChild(h2Element);
-//         let ulEl = document.createElement('ul');
-//         secsalmonCookies.appendChild(ulEl);
-
-//     for(let i = 0; i < hours.length; i++) {
-//         let liElement = document.createElement('li');
-//       liElement.textContent = hours[i]+': '+this.RandomNumOfCust() + 'Cookies';
-//       ulEl.appendChild(liElement);
-//     }
-//     let liElement = document.createElement('li');
-//     liElement.textContent = 'the sum of these hourly totals Cookies: '+this.total+ 'Cookies';
-//     ulEl.appendChild(liElement);
-//     }
-
-// }
-// Tokyo.render();
-
-// const Dubai = {
-//     barchName : 'Dubai',
-//     minCust: 3,
-//     maxCust: 24,
-//     AvgCookie: 1.2,
-//     cookies :0,
-//     total :0 ,
-//     RandomNumOfCust: function() {
-//         let min = Math.ceil(this.minCust);
-//         let max = Math.floor(this.maxCust);
-//         this.cookies = Math.floor((Math.random() * (max - min + 1) + min)*this.AvgCookie);//The maximum is inclusive and the minimum is inclusive
-//      this.total += this.cookies
-//      return this.cookies ;
-//     }, 
-
-
-//     render: function () { 
-
-// let secsalmonCookies = document.getElementById('salmonCookies');
-// let h2Element = document.createElement('h2');
-// h2Element.textContent = this.barchName;
-// secsalmonCookies.appendChild(h2Element);
-//         let ulEl = document.createElement('ul');
-//         secsalmonCookies.appendChild(ulEl);
-
-//     for(let i = 0; i < hours.length; i++) {
-//         let liElement = document.createElement('li');
-//       liElement.textContent = hours[i]+': '+this.RandomNumOfCust() + 'Cookies';
-//       ulEl.appendChild(liElement);
-//     }
-//     let liElement = document.createElement('li');
-//     liElement.textContent = 'the sum of these hourly totals Cookies: '+this.total+ 'Cookies';
-//     ulEl.appendChild(liElement);
-//     }
-
-// }
-// Dubai.render();
-
-
-
-// const Lima = {
-//     barchName : 'Lima',
-//     minCust: 3,
-//     maxCust: 24,
-//     AvgCookie: 1.2,
-//     cookies :0,
-//     total :0 ,
-//     RandomNumOfCust: function() {
-//         let min = Math.ceil(this.minCust);
-//         let max = Math.floor(this.maxCust);
-//         this.cookies = Math.floor((Math.random() * (max - min + 1) + min)*this.AvgCookie);//The maximum is inclusive and the minimum is inclusive
-//      this.total += this.cookies
-//      return this.cookies ;
-//     }, 
-
-
-//     render: function () { 
-
-// let secsalmonCookies = document.getElementById('salmonCookies');
-// let h2Element = document.createElement('h2');
-// h2Element.textContent = this.barchName;
-// secsalmonCookies.appendChild(h2Element);
-//         let ulEl = document.createElement('ul');
-//         secsalmonCookies.appendChild(ulEl);
-
-//     for(let i = 0; i < hours.length; i++) {
-//         let liElement = document.createElement('li');
-//       liElement.textContent = hours[i]+': '+this.RandomNumOfCust() + 'Cookies';
-//       ulEl.appendChild(liElement);
-//     }
-//     let liElement = document.createElement('li');
-//     liElement.textContent = 'the sum of these hourly totals Cookies: '+this.total+ 'Cookies';
-//     ulEl.appendChild(liElement);
-//     }
-
-// }
-// Lima.render();
